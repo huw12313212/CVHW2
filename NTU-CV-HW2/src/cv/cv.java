402824bc6792d1115be06656b80a04c33c6d26b7
王瀚宇 <huw12313212@gmail.com>
2014-10-03 05:54:42 -1000

    Homework Done

--- NTU-CV-HW2/src/cv/cv.java
+++ NTU-CV-HW2/src/cv/cv.java
@@ -11,23 +11,14 @@ import java.util.Hashtable;
 
 public class cv {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException {
-		
-		//
-		//System.out.print("test1");
+
 		 String fileName = "./assets/lena.im";
 		 int headerLength = 172;
 		 int imageWidth = 512;
 		 int imageHeight = 512;
 		 int unit = 1;
 		 int threshold = 128;
-		 
-		 //static final int UNDEFINED = -1;
-		// final int CHECKED = -2;
 		 
 		 //hw1-a
 		 ArrayList<Integer> bytes = GetByteData(fileName);
@@ -45,7 +36,6 @@ public class cv {
 		 }
 		 WriteOut(bytes,"./assets/hw2-a.im");
 		 
-		 
 		 //hw2-b
 		 bytes = GetByteData(fileName);
 		 Hashtable<Integer,Integer> hashtable = new Hashtable<Integer,Integer>();
@@ -59,13 +49,11 @@ public class cv {
 			 for(int x = 0; x <imageWidth;x++)
 			 {
 				 int data = bytes.get(headerLength+(y*imageWidth+x)*unit);
-				 
 				 int oldValue = hashtable.get(data);
 				 int newValue = oldValue + 1;
 				 hashtable.replace(data, newValue);
 			 }
 		 }
-		 
 		 
 		 File f = new File("./assets/hw2-b.csv");
 		 if(f.exists())f.delete();
@@ -81,7 +69,6 @@ public class cv {
 		 }
 		 writer.close();
 		 
-		 //System.out.println("test2");
 		 //hw2-c
 		 bytes = GetByteData(fileName);
 		 for(int y = 0 ; y<imageHeight; y++)
@@ -97,10 +84,7 @@ public class cv {
 			 }
 		 }
 		 
-		// System.out.println("test3");
-		 
-		//Init
-		 
+		 //Init
 		 int i = 0;
 		 ArrayList<Integer> records = new ArrayList<Integer>();
 		 HashMap<Integer,ArrayList<Integer>> GroupMap = new HashMap<Integer,ArrayList<Integer>>();
@@ -119,9 +103,6 @@ public class cv {
 			 }
 		}
 		 
-		// System.out.print(GroupMap);
-		 
-		 
 		 for(int y = 0 ; y<imageHeight; y++)
 		 {
 			 for(int x = 0; x <imageWidth;x++)
@@ -130,10 +111,8 @@ public class cv {
 			 }
 		}
 		
-		
 	     ArrayList<ArrayList<Integer>> candidates = new  ArrayList<ArrayList<Integer>>();
 	     
-		
 		for(int keyValue : GroupMap.keySet())
 		{
 			ArrayList<Integer> alloc = GroupMap.get(keyValue);
@@ -141,7 +120,6 @@ public class cv {
 			if(alloc!=null&&alloc.size()>=500)
 			{
 				candidates.add(GroupMap.get(keyValue));
-			//test +=1;
 				System.out.println("keyValue("+keyValue+"):"+GroupMap.get(keyValue).size());
 			}
 		}
@@ -160,26 +138,21 @@ public class cv {
 		}
 		
 		 WriteOut(bytes,"./assets/hw2-c.im");
-		System.out.println("done");
+		 System.out.println("done");
 	}
 	
 	public static void ReverseColor(ArrayList<Integer> Origin, Rectangle rec)
 	{
-		
 		int centerX = (rec.x1 + rec.x2) /2;
 		int cneterY = (rec.y1 + rec.y2) /2;
 		int ArrowSize = 10;
 		int ArrowWidth = 3;
 		
-		
 		for(int x =rec.x1 ; x<=rec.x2;x++)
 		{
 			for(int y = rec.y1;y<=rec.y2;y++)
 			{
-				
 				int old = Origin.get(172+ y*512+x);
-				
-				
 				if((x >= centerX - ArrowSize &&x<=centerX + ArrowSize&& y >= cneterY - ArrowWidth && y <= cneterY + ArrowWidth)||
 				   (x >= centerX - ArrowWidth &&x<=centerX + ArrowWidth&& y >= cneterY - ArrowSize && y <= cneterY + ArrowSize)	)
 				{
@@ -201,16 +174,10 @@ public class cv {
 		rec.x1 = rec.x2 = candidate.get(0)%512;
 		rec.y1 = rec.y2 = (int) Math.floor(candidate.get(0)/512);
 	
-		
-		//for(int i =0;i<)
-		
-		
 		System.out.println("size:"+candidate.size());
 		
 		for(Integer i : candidate)
 		{
-			//System.out.println(i);
-			
 			int x = i%512;
 			int y = (int) Math.floor(i/512);
 			
@@ -221,13 +188,8 @@ public class cv {
 			if(y>rec.y2)rec.y2 = y;
 		}
 		
-		
 		return rec;
 	}
-
-	
-	
-	
 	
 	static public void connectMapCheck(ArrayList<Integer> Origin,ArrayList<Integer> records,HashMap<Integer,ArrayList<Integer>> GroupMap,int x,int y)
 	{
@@ -237,8 +199,6 @@ public class cv {
 		int unit = 1;
 		int threshold = 128;
 		
-		
-		
 		if(Origin.get(headerLength+(y*imageWidth+x))==0)
 		{
 			GroupMap.get(x+y*imageWidth).clear();
@@ -247,7 +207,6 @@ public class cv {
 		}
 		else
 		{	
-			
 			if(y-1>0)
 			{
 				if(Origin.get(headerLength+(x+(y-1)*imageWidth))!=0)
@@ -282,8 +241,6 @@ public class cv {
 		
 		if(killArray == biggerArray) return;
 		
-
-		
 		//refreashRecord
 		for(int i= 0;i<killArray.size();i++)
 		{
@@ -294,7 +251,6 @@ public class cv {
 		GroupMap.replace(origin,null);
 		killArray.clear();
 		
-		
 		System.out.println(biggerArray.size());
 		
 	}
@@ -302,9 +258,7 @@ public class cv {
 	public static void WriteOut(ArrayList<Integer> data,String name) throws IOException
 	{
 		File f = new File(name);
-		
 		if(f.exists())f.delete();
-		
 		FileOutputStream out = null;
 		out = new FileOutputStream(name);
 		
@@ -328,8 +282,6 @@ public class cv {
 	
 	public static ArrayList<Integer> GetByteData(String fileName) throws IOException
 	{
-		
-		
 		 File f = new File(fileName);
 		 ArrayList<Integer> bytes = new ArrayList<Integer>();
 		
@@ -345,7 +297,4 @@ public class cv {
 		 
 		 return bytes;
 	}
-	
-	
-
 }
